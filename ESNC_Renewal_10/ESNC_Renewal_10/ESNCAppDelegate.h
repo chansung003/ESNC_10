76dde18b8779b22667325cea5cb68fe5002135c3
chansung18 <chansung18@gmail.com>
2013-04-26 22:07:37 +0900

    project check in (initialisation)

---
+++ ESNC_Renewal_10/ESNC_Renewal_10/ESNCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ESNCAppDelegate.h
+//  ESNC_Renewal_10
+//
+//  Created by Chansung, Park on 4/26/13.
+//  Copyright (c) 2013 Chansung, Park. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ESNCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ESNC_Renewal_10/ESNC_Renewal_10/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ESNC_Renewal_10
+//
+//  Created by Chansung, Park on 4/26/13.
+//  Copyright (c) 2013 Chansung, Park. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ESNCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ESNCAppDelegate class]));
+    }
+}
